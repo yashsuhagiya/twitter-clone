a2b20196db9356b157132a24da49cc59a115562d
yashsuhagiya <yashsuhagiya401@gmail.com>
2023-06-28 20:16:04 +0200

    style change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,11 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <ClerkProvider {...pageProps}>
+  return (
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
     </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
